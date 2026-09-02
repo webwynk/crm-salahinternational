038a82cf1e76befa164e29326a49f6683731f320
webwynke <webwynk@gmail.com>
2026-09-03 01:45:29 +0530

    chore: update build assets for leather category custom input

---
+++ public/build/assets/units-CVx095Ru.js
@@ -0,0 +1,11 @@
+import{c as e}from"./Button-Cu-IEcfo.js";/**
+ * @license lucide-react v1.27.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["path",{d:"M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z",key:"vktsd0"}],["circle",{cx:"7.5",cy:"7.5",r:".5",fill:"currentColor",key:"kqv944"}]],s=e("tag",l);/**
+ * @license lucide-react v1.27.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M10 11v6",key:"nco0om"}],["path",{d:"M14 11v6",key:"outv1u"}],["path",{d:"M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6",key:"miytrc"}],["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2",key:"e791ji"}]],u=e("trash-2",t),c=[{value:"sq_ft",label:"Square Feet (sq_ft)"},{value:"pcs",label:"Pieces (pcs)"},{value:"m",label:"Meter (m)"},{value:"cm",label:"Centimeter (cm)"},{value:"in",label:"Inch (in)"},{value:"yard",label:"Yard (yard)"},{value:"feet",label:"Feet (feet)"},{value:"sq m",label:"Square Meter (sq m)"},{value:"sq_dm",label:"Square Decimeter (sq_dm)"},{value:"hides",label:"Hides / Sides (hides)"},{value:"cm2",label:"Square Centimeter (cm2)"},{value:"g",label:"Gram (g)"}];c.map(a=>a.value);export{c as B,s as T,u as a};
